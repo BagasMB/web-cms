f2936f3c296db38e900d8222407201fdc70294ae
bgsmhrdka_ <bagasmahardikabudi2007@gmail.com>
2023-12-06 09:19:40 +0700

    Initial commit

--- application/views/galeri.php
+++ application/views/galeri.php
@@ -7,19 +7,17 @@
                     </div>
                 </div>
                 <?php foreach ($galeri as $ten) : ?>
-                    <div class="col-lg-3 col-sm-6 mb-4">
-                        <article class="mb-5">
-                            <div class="post-slider slider-sm">
-                                <a href="<?= base_url('assets/upload/galeri/' . $ten['foto']); ?>" class="fancybox" data-fancybox="gallery">
-                                    <img src="<?= base_url('assets/upload/galeri/' . $ten['foto']); ?>" class="img-fluid" alt="post-thumb">
-                                </a>
-                            </div>
-                            <h3 class="h5 post-title"><?= word_limiter($ten['judul'], 5);  ?>.</h3>
-                            <ul class="list-inline post-meta mb-2">
-                                <li class="list-inline-item">Date : <?= $ten['tanggal']; ?></li>
-                            </ul>
-                        </article>
-                    </div>
+                    <article class="col-lg-3 col-sm-6">
+                        <div class="post-slider slider-sm">
+                            <a href="<?= base_url('assets/upload/galeri/' . $ten['foto']); ?>" class="fancybox" data-fancybox="gallery">
+                                <img src="<?= base_url('assets/upload/galeri/' . $ten['foto']); ?>" class="img-fluid" alt="post-thumb">
+                            </a>
+                        </div>
+                        <!-- <h3 class="h5 post-title"><?= word_limiter($ten['judul'], 5);  ?>.</h3>
+                        <ul class="list-inline post-meta mb-2">
+                            <li class="list-inline-item">Date : <?= $ten['tanggal']; ?></li>
+                        </ul> -->
+                    </article>
                 <?php endforeach; ?>
             </div>
         </div>
